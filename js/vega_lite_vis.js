ae425da382f187639f35dfa680da0ccc9927f7c1
lbul0011 <lbul0011@student.monash.edu>
2025-10-10 22:35:54 +1100

    week 10 homework task

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,21 @@
 var vg_1 = "js/symbol_map.vg.json";
+var vg_2 = "js/stacked_bar_chart.vg.json";
+var vg_3 = "js/test_stacked_area.vg.json";
+var vg_4 = "js/scatter_legend.vg.json";
+
+
 vegaEmbed("#symbol_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#stacked_bar_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bar_chart_1", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#scatter_legend", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
