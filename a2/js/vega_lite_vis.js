a59c115d35668fb22f50b537021225dc5c6fb68f
lbul0011 <lbul0011@student.monash.edu>
2025-10-14 22:48:40 +1100

    a2 upload

---
+++ a2/js/vega_lite_vis.js
@@ -0,0 +1,28 @@
+var vg_1 = "js/symbol_map.vg.json";
+var vg_2 = "js/stacked_bar_chart.vg.json";
+var vg_3 = "js/test_stacked_area.vg.json";
+var vg_4 = "js/line_chart.vg.json";
+var vg_5 = "js/dot_plot.vg.json";
+var vg_6 = "js/scatter_legend.vg.json";
+var vg_7 = "js/heatmap.vg.json";
+
+vegaEmbed("#symbol_map", vg_1,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#stacked_bar_chart", vg_2,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#bar_chart_1", vg_3,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#line_chart", vg_4,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#connected_dot_plot", vg_5,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#scatter_legend", vg_6,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#heatmap", vg_7,{"actions": false}).then(function(result) {
+}).catch(console.error);
