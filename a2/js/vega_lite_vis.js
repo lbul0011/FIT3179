621be48d8ff94cd80ed4ad51a04d27088bb10fa8
lbul0011 <lbul0011@student.monash.edu>
2025-10-20 16:51:56 +1100

    update a2

---
+++ a2/js/vega_lite_vis.js
@@ -0,0 +1,40 @@
+var vg_1 = "js/symbol_map.vg.json";
+var vg_2 = "js/stacked_bar_chart.vg.json";
+var vg_3 = "js/test_stacked_area.vg.json";
+var vg_4 = "js/line_chart.vg.json";
+var vg_5 = "js/dot_plot.vg.json";
+var vg_6 = "js/scatter_legend.vg.json";
+var vg_7 = "js/heatmap.vg.json";
+var vg_8 = "js/area_chart.vg.json";
+var vg_9 = "js/line_chart_2.vg.json";
+var vg_10 = "js/zoomed_in_map.vg.json";
+
+vegaEmbed("#symbol_map", vg_1,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#stacked_bar_chart", vg_2,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#area_chart", vg_3,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#punctuality_line_chart", vg_4,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#connected_dot_plot", vg_5,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#scatter_legend", vg_6,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#heatmap", vg_7,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#area_chart_2", vg_8,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#punctuality_line_chart_2", vg_9,{"actions": false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#zoomed_in_map", vg_10,{"actions": false}).then(function(result) {
+}).catch(console.error);
